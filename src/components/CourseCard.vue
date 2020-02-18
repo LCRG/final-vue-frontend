0edd36b2559710c2e7c2103d0bf6917e3e1b4f56
Thor Anderson <ta.anderson@gmail.com>
2020-02-18 16:33:59 -0700

    More UI

---
+++ src/components/CourseCard.vue
@@ -0,0 +1,25 @@
+<template>
+   <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      course: {}
+    }
+  },
+  methods: {
+    getCourseData() {
+
+    }
+  }
+}
+</script>
---
+++ src/components/ProgramView.vue
@@ -0,0 +1,206 @@
+<template>
+  <v-container fluid class="grey lighten-4 ma-4">
+    <v-row dense>
+      <v-col cols="12">
+        <v-row 
+          class="grey lighten-4"
+          style="width: 100%"
+        >
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+        </v-row>
+      </v-col>
+      <v-col cols="12">
+        <v-row
+          class="grey lighten-4"
+          style="width: 100%;"
+        >
+        
+          <v-spacer></v-spacer>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+        </v-row>
+      </v-col>
+      <v-col cols="12">
+        <v-row
+          class="grey lighten-4"
+          style="width: 100%;"
+        >
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+        </v-row>
+      </v-col>
+      <v-col cols="12">
+        <v-row
+          class="grey lighten-4"
+          style="width: 100%;"
+        >
+        <v-spacer></v-spacer>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+        </v-row>
+      </v-col>
+      <v-col cols="12">
+        <v-row
+          class="grey lighten-4"
+          style="width: 100%;"
+        >
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-spacer></v-spacer>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+        </v-row>
+      </v-col>
+      <v-col cols="12">
+        <v-row
+          class="grey lighten-4"
+          style="width: 100%;"
+        >
+          
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+          <v-card class="ma-2 pa-4" outlined tile>
+            <v-card-title>DGM 2120 </v-card-title>
+            <v-card-subtitle>
+              Web Essentials
+            </v-card-subtitle>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ProgramView',
+
+  data: () => ({})
+}
+</script>
